0398938bb94176fb52c6a8dcaf7b5bf6b3fdd5f4
Harish5nu <saudharish770@gmail.com>
2025-07-14 22:13:12 +0545

    added new problem

---
+++ problem2.py
@@ -0,0 +1,8 @@
+q=int(input("Enter your age:"))
+if(q>=18):
+    print("You are not in teen age")
+
+elif(q<0):
+    print("You are entering invalid age/ age cannot be under 0")
+else:
+    print("You are teenager")
