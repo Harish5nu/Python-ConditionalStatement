b795038a0c46ec558441502b64fc544443593a14
Harish Saud <saudharish770@gmail.com>
2025-09-01 17:13:01 +0545

    Add files via upload

--- problem2.py
+++ problem2.py
@@ -1,17 +1,14 @@
+#Q. write a program to find out whether a student has passed or failed if it requires a total of 40% and at least 33% in each subject to pass. Assume 3 subjects and take marks as an input from the user.
 # Take marks input from user for 3 subjects
 s1 = int(input("Enter marks for Subject 1: "))
 s2 = int(input("Enter marks for Subject 2: "))
 s3 = int(input("Enter marks for Subject 3: "))
 
-# Calculate total and average
-total = s1 + s2 + s3
-average = total / 3
+#checks for total percentage
+total_percentage=(100*(s1+s2+s3))/300
 
-# Check pass/fail conditions
-if s1 >= 33 and s2 >= 33 and s3 >= 33:
-    if average >= 40:
-        print("Student has passed.")
-    else:
-        print("Student has failed due to less than 40% overall.")
+if(total_percentage>=40 and s1>=33 and s2>=33 and s3>=33):
+    print("You passed", total_percentage)
+
 else:
-    print("Student has failed due to less than 33% in one or more subjects.")
+    print("You failed try again", total_percentage)
