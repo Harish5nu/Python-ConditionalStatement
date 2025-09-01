655e5aa2fa253b6d20fa1c562e1e1cc6703e09cb
Harish Saud <saudharish770@gmail.com>
2025-09-02 00:51:31 +0545

    Add files via upload

--- problem3.py
+++ problem3.py
@@ -1,22 +1,13 @@
-# A longer but simple Python conditional Program
+#Q. A  spam comment if defined as a text containing following keywords:
+# "Make a lot of money", " buy now", "subscribe this", " click this ". write a program to detect these spams.
 
-marks = int(input("Enter your marks: "))
+s1="Make a lot of money" 
+s2="buy now"
+s3="subscribe this"
+s4="click this"
+message=input("Enter your comment: ")
 
-if marks >= 90:
-    print("Grade: A+")
-    print("Excellent performance!")
-elif marks >= 75:
-    print("Grade: A")
-    print("Very Good! Keep it up.")
-elif marks >= 60:
-    print("Grade: B")
-    print("Good work, but you can improve.")
-elif marks >= 45:
-    print("Grade: C")
-    print("You passed, but study harder.")
-elif marks >= 30:
-    print("Grade: D")
-    print("You barely passed. Try again with more focus.")
+if ((s1 in message) or (s2 in message) or (s3 in message) or (s4 in message)):
+    print("This is spam")
 else:
-    print("Grade: F")
-    print("You failed. Don’t give up, study and try again!")
+    print("This is not spam")
