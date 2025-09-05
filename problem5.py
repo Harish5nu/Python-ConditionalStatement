e7f7e56a201a9fb08efb0dfcafc7912d414807dc
Harish Saud <saudharish770@gmail.com>
2025-09-05 12:15:26 +0545

    Add files via upload

---
+++ problem5.py
@@ -0,0 +1,8 @@
+#Q. Write a program which finds out whether a given name is present in a list or not.
+list=["Harry", "Abishek","Garima","Puja","Rabina"]
+yourname=input("Enter your name:")
+
+if (yourname in list):
+    print("You are in the list")
+else:
+    print("You are not in the list..")
