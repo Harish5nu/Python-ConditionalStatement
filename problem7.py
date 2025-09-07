a6100643092e595c72d334deaf9c6d0e5cd88fa6
Harish Saud <saudharish770@gmail.com>
2025-09-07 10:33:05 +0545

    Add files via upload

---
+++ problem7.py
@@ -0,0 +1,7 @@
+#Q. write a program to find out whether a given post is taking about "Harry" or not.
+post=input("Enter the post: ")
+
+if("Harry" .lower() in post.lower()):
+    print("This post is taking about harry")
+else:
+    print("This is not taking about harry")
